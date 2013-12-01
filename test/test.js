42ed27abd6c33fe51b57cf9ea0bea8bd843410fc
DonaldIsFreak <ron81018@gmail.com>
2013-12-01 21:41:57 +0800

    Update the test.js

--- test/test.js
+++ test/test.js
@@ -4,32 +4,36 @@ var expect = require("expect.js");
 
 
 describe('Checking Express RESTful api server,',function(){
-	var _isbn = '001',_title='A new books',_description='None';
+	var _id='',_isbn = '001',_title='A new books',_description='None';
 
 	it('post a new book',function(done){
 		superagent.post('http://localhost:8080/books/')
 			.send({
-				isbn : _isbn,
-				title : _title, 
-				description: _description 
+				book:{
+					isbn : _isbn,
+					title : _title, 
+					description: _description 
+				}
 			})
 			.end(function(e,res){
 				expect(e).to.eql(null);
-				expect(res.body).to.be.an('object');
-				expect(res.body.isbn).to.eql(_isbn);
-				expect(res.body.title).to.eql(_title);
-				expect(res.body.description).to.eql(_description);
-				expect(res.body._id.length).to.eql(24);
+				expect(res.body.book).to.be.an('object');
+				expect(res.body.book.isbn).to.eql(_isbn);
+				expect(res.body.book.title).to.eql(_title);
+				expect(res.body.book.description).to.eql(_description);
+				expect(res.body.book._id.length).to.eql(24);
 				done();
+				_id = res.body.book._id;
 			});
 	});
+
 	it('retrieves one book info',function(done){
-		superagent.get('http://localhost:8080/books/'+_isbn)
+		superagent.get('http://localhost:8080/books/'+_id)
 			.end(function(e,res){
 				expect(e).to.eql(null);
-				expect(res.body).to.be.an('object');
-				expect(res.body.isbn).to.eql(_isbn);
-				expect(res.body._id.length).to.eql(24);
+				expect(res.body.book).to.be.an('object');
+				expect(res.body.book.isbn).to.eql(_isbn);
+				expect(res.body.book._id.length).to.eql(24);
 				done();
 			});
 	});
@@ -38,47 +42,45 @@ describe('Checking Express RESTful api server,',function(){
 		superagent.get('http://localhost:8080/books')
 			.end(function(e,res){
 				expect(e).to.eql(null);
-				expect(res.body.length).to.be.above(0);
-				expect(res.body.map(function(item){
+				expect(res.body.book.length).to.be.above(0);
+				expect(res.body.book.map(function(item){
 					return item.isbn;
 				})).to.contain(_isbn);
 				done();
 			});
 	});
 
-
 	it('updates a book info',function(done){
-		superagent.put('http://localhost:8080/books/'+_isbn)
+		superagent.put('http://localhost:8080/books/'+_id)
 			.send({
-				description: 'Modify book description'
+				book: {
+					description: 'Modify book description'
+				}
 			})
 			.end(function(e,res){
 				expect(e).to.eql(null);
-				expect(res.body).to.be.an('object');
-				expect(res.body.msg).to.eql('success');
+				expect(res.status).to.eql(200);
 				done();
 			});
 	});
 
 	it('check book info after update',function(done){
-		superagent.get('http://localhost:8080/books/'+_isbn)
+		superagent.get('http://localhost:8080/books/'+_id)
 			.end(function(e,res){
 				expect(e).to.eql(null);
-				expect(res.body).to.be.an('object');
-				expect(res.body._id.length).to.eql(24);
-				expect(res.body.isbn).to.eql(_isbn);
-				expect(res.body.title).to.eql(_title);
-				expect(res.body.description).not.to.eql(_description);
+				expect(res.body.book).to.be.an('object');
+				expect(res.body.book._id.length).to.eql(24);
+				expect(res.body.book.isbn).to.eql(_isbn);
+				expect(res.body.book.title).to.eql(_title);
+				expect(res.body.book.description).not.to.eql(_description);
 				done();
 			});
 	});
-
 	it('remove a book',function(done){
-		superagent.del('http://localhost:8080/books/'+_isbn)
+		superagent.del('http://localhost:8080/books/'+_id)
 			.end(function(e,res){
 				expect(e).to.eql(null);
-				expect(res.body).to.be.an('object');
-				expect(res.body.msg).to.eql('success');
+				expect(res.status).to.eql(200);
 				done();
 			});
 	});
