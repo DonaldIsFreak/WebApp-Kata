72146a56f3471151347c7f89fbe458b386f2b56b
DonaldIsFreak <ron81018@gmail.com>
2014-02-01 17:38:46 +0800

    Add the grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,25 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg:    grunt.file.readJSON('package.json'),
 
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: ['**'],
+                        dest: 'public/',
+                    },
+                    {
+                        expand: true,
+                        cwd: 'vendor/',
+                        src: ['**'],
+                        dest: 'public/',
+                    }
+                ]
+            }
+        },
+
         jshint: {
             options: {
                 jshintrc: '.jshintrc'
@@ -10,9 +29,14 @@ module.exports = function(grunt) {
             src: [
                 "server.js",
                 "app/*/*.js",
-                "public/js/*.js",
+                "src/js/*.js",
                 "test/*.js"
             ]
+        },
+
+        bower: {
+            install: {
+            }
         },
 
         mochaTest: {
@@ -23,11 +47,13 @@ module.exports = function(grunt) {
                 'test/**/*.js'
             ]
         },
-});
+    });
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-bower-task');
 
-    grunt.registerTask('test',['mochaTest']);
-    grunt.registerTask('default',['jshint','mochaTest']);
+    grunt.registerTask('test',['copy','bower','mochaTest']);
+    grunt.registerTask('default',['copy','bower','jshint']);
 };
