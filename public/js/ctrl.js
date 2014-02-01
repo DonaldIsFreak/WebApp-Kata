b4816905265855ede24254e0d8a3c0abfc95eb90
DonaldIsFreak <ron81018@gmail.com>
2014-02-01 08:48:43 +0800

    Format style again

--- public/js/ctrl.js
+++ public/js/ctrl.js
@@ -1,45 +1,45 @@
 App.BooksController = Ember.ArrayController.extend({
-	actions: {
-		add: function(){
-			var title = this.get('newTitle');
-			var isbn = this.get('newISBN');
-			var description = this.get('newDesc');
-			var book = this.store.createRecord('book',{
-				title : title,
-				isbn : isbn,
-				description : description
-			});
-			book.save();
-			this.set('newTitle','');
-			this.set('newISBN','');
-			this.set('newDesc','');
-		}
-	}
+    actions: {
+        add: function(){
+            var title = this.get('newTitle');
+            var isbn = this.get('newISBN');
+            var description = this.get('newDesc');
+            var book = this.store.createRecord('book',{
+                title : title,
+                isbn : isbn,
+                description : description
+            });
+            book.save();
+            this.set('newTitle','');
+            this.set('newISBN','');
+            this.set('newDesc','');
+        }
+    }
 });
 
 App.BookController = Ember.ObjectController.extend({
-	isEditing: false,
+    isEditing: false,
     isRevming: false,
-	actions: {
-		editDesc: function(){
-			this.set('isEditing',true);
-		},
-		remove: function(){
+    actions: {
+        editDesc: function(){
+            this.set('isEditing',true);
+        },
+        remove: function(){
             this.toggleProperty('isRemoving');
-		},
-		update: function(){
-			this.set('isEditing',false);
-			this.get('model').save();
-		},
+        },
+        update: function(){
+            this.set('isEditing',false);
+            this.get('model').save();
+        },
         confirmRemove: function(){
-			var books = this.get('model');
-			books.deleteRecord();
-			books.save();
+            var books = this.get('model');
+            books.deleteRecord();
+            books.save();
             this.set('isRemoving',false);
         },
         cancelRemove: function(){
             this.set('isRemoving',false);
-		}
-	}
+        }
+    }
 });
 
